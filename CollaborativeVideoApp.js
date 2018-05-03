063bf9933f2f5c1f01ef9678fcd7445d86b545a0
Luxapodular <luca.damasco@gmail.com>
2018-05-02 22:16:45 -0400

    Fix num words.

--- CollaborativeVideoApp.js
+++ CollaborativeVideoApp.js
@@ -6,26 +6,26 @@ var state = 1;
 
 function countWords(str) {
   var words = str.trim().split(/\s+/).length;
+  return words; 
 }
 
 
 function myFunction() {
     if (state === 1) {
         UserQuestion = document.getElementById("textArea").value;
-        countWords(UserQuestion);
+        var numWords = countWords(UserQuestion);
+      
         if (UserQuestion.length < 10) {
             alert("Questions must be at least 10 characters long");
-        }
-        else if (words < 4) {
+        } else if (numWords < 4) {
             alert("Questions must be at least 4 words long");
-        }
-        else {
+        } else {
             document.getElementById("textArea").value = "";
             document.getElementById("textArea").placeholder = "Write answer";
             document.getElementById("prompt").innerHTML = "Answer?";
             state = state + 1;
         }
-        }
+    }
     else if (state === 2) {
         Answer1 = document.getElementById("textArea").value;
         document.getElementById("textArea").value = "";
@@ -38,12 +38,6 @@ function myFunction() {
         document.getElementById("prompt").value = "Elaborated answer?";
 
     }
-        
-        
-    
-    
-    
-    
 }
 
 
